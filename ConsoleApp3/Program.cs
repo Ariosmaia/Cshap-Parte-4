c207f6e9df9c7b194d4f7f66b6d535346780cd1b
Allan Rios <ariosmaia@yahoo.com.br>
2018-09-10 18:10:30 -0300

    ArgumentException

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                //Metodo();
+
+                ContaCorrente conta = new ContaCorrente(514, 0);
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Não é possivel divisão por zero");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine("Aconteceu um erro");
+            }
+            
+
+            Console.ReadLine();
+        }
+        //Teste com a cadeia de chamada:
+        //Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+
+                Console.WriteLine($"Exceção com numuro= {numero} e divisor {divisor}");
+                throw;
+            }
+
+            
+        }
+    } 
+}
